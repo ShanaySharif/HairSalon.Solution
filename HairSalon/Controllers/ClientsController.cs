095cf739fd82294706ad25e121af3d62ee5d3bd5
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-28 10:21:11 -0400

    project set up

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,16 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using  HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+    public class ClientsController : Controller
+    {
+
+    }
+}
---
+++ HairSalon/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using HairSalon.Models;
+
+namespace HairSalon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<FavRestaurantsContext>(
+                dbContextOptions => dbContextOptions.UseMySql(
+                    builder.Configuration["ConnectionStrings:DefaultConnection"],
+                    ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+                )
+            );
+            WebApplication app = builder.Build();
+
+            // app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+            app.Run();
+        }
+    }
+}
